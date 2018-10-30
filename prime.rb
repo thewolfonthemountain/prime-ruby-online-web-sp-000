3056dcf551133458af92a735cd57a5f535504cf3
Sean Langton <slangton1@gmail.com>
2018-10-30 01:57:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(integer)
 return true if integer == 2
-return false if integer < 2
+return false if integer < 0
 array = 2..integer-1
 array.map {|number| integer%number}
 array.include?(0)
