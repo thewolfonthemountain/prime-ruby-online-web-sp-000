510e5800f7ba29e39d515fd4ef37b01cc5d28c20
Sean Langton <slangton1@gmail.com>
2018-10-30 02:07:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(integer)
+if integer < 2 then false
+elsif integer == 2 then true
+elsif integer == 3 then true
+else
+array = 2..integer-1
+!array.any? {|number| integer%number == 0}
+end
+
+end
