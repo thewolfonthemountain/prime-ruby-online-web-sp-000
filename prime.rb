63dea3952ffe434314e85871602a2947fbacf9f6
Sean Langton <slangton1@gmail.com>
2018-10-30 02:06:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ elsif integer == 2 then true
 elsif integer == 3 then true
 else
 array = 2..integer-1
-array.any? {|number| integer%number = 0}
+array.any? {|number| integer%number == 0}
 end
 
 end
